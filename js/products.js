463015fab201464d4d37bc16062dd39cb2f78a47
aniggo <anaclara.ggomez@gmail.com>
2021-08-16 17:16:37 -0300

    modificacion apariencia login

--- js/products.js
+++ js/products.js
@@ -2,5 +2,12 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+    let url="https://japdevdep.github.io/ecommerce-api/product/all.json";
+   fetch(url)
+    .then (result => result.json())
+    .then (data => {
+        let nombre= data[0].name;
+        console.log(nombre);
+        document.getElementById("auto1").textContent= nombre;
+    });
 });
